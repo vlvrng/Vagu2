0785d921cff534340b1d2c65208ec0174b7369bc
vlvrng <vlvrng@gmail.com>
2021-07-26 14:03:12 -0300

    Ajustes html, css, js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ const form = document.getElementById("form")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("e-mail").value;
-    let data = {
-        nome,
-        email,
+    let CPF OU EMAIL = document.getElementById("CPF OU EMAIL").value;
+    let SENHA = document.getElementById("SENHA").value;
+    let CPF OU EMAIL = {
+        SENHA,
     }
     let convertData = JSON.stringify(data);
 
     localStorage.setItem("lead", convertData)
     
-    alert("Cadastro efetuado com sucesso!");
+    alert("Acesso efetuado com sucesso!");
 })
